ea8c1735b8df1fcc838dbe716caf4b00b09f9600
Edwin <60476129+Edwinexd@users.noreply.github.com>
2023-03-21 20:49:11 +0100

    Support non-ascii in clipboard & Automatically download tesseract model

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # AnyComplete - Complete messages anywhere with GPT
 AnyComplete is a tool designed to make typing easier by predicting what the user is going to type next. Uses Tesseract OCR to OCR the current application window, then passes it through GPT3.5-turbo along with text that has been entered to predict the rest of a message. The program is bound to the shortcut win-shift-a and works on any application window where the textbox support ctrl a & ctrl c.
 
-# Usage
-AnyComplete is written in Rust and requires Leptess to be set up correctly. Please follow the instructions on the [Leptess](https://github.com/houqp/leptess) page to set up Leptess for use with Rust.
+# Building
+AnyComplete is written in Rust and requires Leptess to be set up correctly. Please follow the instructions on the [Leptess](https://github.com/houqp/leptess) before building AnyComplete.
 
-To build AnyComplete, run `cargo build --release` in the project directory.
+Building AnyComplete can be done with cargo by running `cargo build --release` in the project directory.
 
-AnyComplete requires an OpenAI API key to be set in a dotenv file in the project directory. The file should be named `.env` and contain the following line:
-```OPENAI_API_KEY=<your api key>```
+# Running
+## Configuring
+To use AnyComplete, you need to set your OpenAI API key in a `.env` file located in the same directory as the AnyComplete executable. The `.env` file should contain the following line:
+```OPENAI_API_KEY=sk-yourkeyhere```
 
-# Usage
-Once AnyComplete is built, simply press win-shift-a while typing in any application window with text input. The program will use OCR to capture the current window and predict what you're going to type next. The result will be pasted next to the text you've already entered.
+## Usage
+Once AnyComplete is built and running, simply press win-shift-a while typing in any application window with text input. The program will use OCR to capture the current window and predict what you're going to type next. The result will be pasted next to the text you've already entered.
 
 # Known limitations
-- Non-ASCII characters doesn't get pasted correctly
 - Only works on Windows
 - Model and language are hardcoded.
 - The program may crash if the openai api call fails.
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,23 @@ use enigo::Enigo;
 use enigo::Key;
 use enigo::KeyboardControllable;
 use leptess::LepTess;
+use reqwest::Url;
+use directories::{ProjectDirs};
 use win_screenshot::prelude::capture_window;
 use win_screenshot::prelude::Area;
 use image::{ImageBuffer, Rgba};
-use winapi::shared::ntdef::LPCSTR;
-use winapi::um::winbase::{GMEM_MOVEABLE, GlobalLock, GlobalUnlock};
-use winapi::um::winuser::{CF_TEXT, CloseClipboard, EmptyClipboard, GetClipboardData, OpenClipboard, SetClipboardData};
+use winapi::um::winbase::{GMEM_MOVEABLE, GlobalLock, GlobalUnlock, GlobalAlloc, GlobalSize};
+use winapi::um::winuser::CF_UNICODETEXT;
+use winapi::um::winuser::{CloseClipboard, EmptyClipboard, GetClipboardData, OpenClipboard, SetClipboardData};
 use std::env;
-use std::ffi::CStr;
-use std::ffi::CString;
+use std::ffi::OsStr;
+use std::ffi::OsString;
 use std::io::Cursor;
+use std::os::windows::prelude::OsStrExt;
+use std::os::windows::prelude::OsStringExt;
+use std::path::PathBuf;
 use std::ptr;
+use std::sync::Mutex;
 use chat_gpt_rs::prelude::*;
 use active_win_pos_rs::get_active_window;
 use dotenv::dotenv;
@@ -43,19 +49,27 @@ fn capture_window_screenshot() -> ImageBuffer<Rgba<u8>, Vec<u8>> {
     return img;
 }
 
-fn perform_ocr(img: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> String {
-    let mut tiff_buffer = Vec::new();
-    img.write_to(
-        &mut Cursor::new(&mut tiff_buffer),
-        image::ImageOutputFormat::Tiff,
-    )
-    .unwrap();
+struct OCR {
+    leptess: LepTess,
+}
+impl OCR {
+    fn new(data_path: &str, language: &str) -> Self {
+        let leptess = LepTess::new(Some(data_path), language).unwrap();
+        Self { leptess }
+    }
+    
+    fn perform_ocr(&mut self, img: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> String {
+        let mut tiff_buffer = Vec::new();
+        img.write_to(
+            &mut Cursor::new(&mut tiff_buffer),
+            image::ImageOutputFormat::Tiff,
+        )
+        .unwrap();
 
-    let mut ocr = LepTess::new(None, "eng").unwrap();
+        self.leptess.set_image_from_mem(&tiff_buffer).unwrap();
 
-    ocr.set_image_from_mem(&tiff_buffer).unwrap();
-
-    ocr.get_utf8_text().unwrap()
+        self.leptess.get_utf8_text().unwrap()
+    }
 }
 
 fn execute_ctrl_a_c() {
@@ -82,7 +96,7 @@ fn read_clipboard() -> Option<String> {
         }
 
         // Get a handle to the clipboard data
-        let handle = GetClipboardData(CF_TEXT);
+        let handle = GetClipboardData(CF_UNICODETEXT);
         if handle.is_null() {
             CloseClipboard();
             return None;
@@ -97,18 +111,15 @@ fn read_clipboard() -> Option<String> {
         }
 
         // Convert the clipboard data to a string
-        let string = CStr::from_ptr(ptr as LPCSTR)
-            .to_string_lossy()
-            .into_owned();
+        let wide = std::slice::from_raw_parts(ptr as *const u16, (GlobalSize(handle) / 2) as usize);
+        let string = OsString::from_wide(wide).into_string().unwrap_or_default();
 
         // Unlock the handle and close the clipboard
         GlobalUnlock(handle);
         CloseClipboard();
-
         Some(string)
     }
 }
-
 // Complete the given text and context with openai chatgpt
 async fn complete_text(text: &str, context: &str) -> String {
     let token = Token::new(env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set"));
@@ -117,7 +128,12 @@ async fn complete_text(text: &str, context: &str) -> String {
         model: Model::Gpt35Turbo,
         messages: vec![Message {
             role: "system".to_string(),
-            content: "You are a text completion bot.\nYou expect to receive two messages: an OCR read of the application window and the user's text to be completed.\nIf the user includes \"#\" followed by instructions in their text, You should consider any instructions that follow it.\nYou should only append to the user's existing text to complete it, starting where the user left off.\nYou should not add new information beyond what is in the OCR read or user's provided text.\nYou should not reference yourself in its responses.".to_string(),
+            content: "You are a text completion bot designed to assist users with writing messages. Abide by the following:
+            1. Don't erase or edit anything the user has already typed
+            2. Don't repeat anything the user has already typed, instead, continue where they left off
+            3. Try to only use information available in the OCR
+            4. Always do the best of your ability to autocomplete the user's message even if you are missing information.
+            ".to_string(),
         }, Message {
             role: "user".to_string(),
             content: format!("OCR: {}", context),
@@ -141,18 +157,17 @@ fn update_clipboard(content: &str) {
         if OpenClipboard(ptr::null_mut()) == 0 {
             return;
         }
-        
         EmptyClipboard();
-        
-        let cstring = CString::new(content).unwrap();
-        let size = (cstring.as_bytes().len() + 1) as usize;
-        let handle = winapi::um::winbase::GlobalAlloc(GMEM_MOVEABLE, size);
-        let data = winapi::um::winbase::GlobalLock(handle) as *mut u8;
-        ptr::copy_nonoverlapping(cstring.as_bytes().as_ptr(), data, size);
-        winapi::um::winbase::GlobalUnlock(handle);
-        
-        SetClipboardData(CF_TEXT, handle);
-        
+
+        let wide: Vec<u16> = OsStr::new(content).encode_wide().chain(Some(0)).collect();
+        let size = wide.len() * 2;
+        let handle = GlobalAlloc(GMEM_MOVEABLE, size);
+        let data = GlobalLock(handle) as *mut u16;
+        ptr::copy_nonoverlapping(wide.as_ptr(), data, wide.len());
+        GlobalUnlock(handle);
+
+        SetClipboardData(CF_UNICODETEXT, handle);
+
         CloseClipboard();
     }
 }
@@ -164,30 +179,60 @@ fn paste_clipboard() {
     enigo.key_up(Key::Control);
 }
 
-fn main() {
+fn get_tessdata_path() -> PathBuf {
+    let mut data_path = PathBuf::from(ProjectDirs::from("com", "Edwinexd", "AnyComplete").unwrap().data_dir().to_str().unwrap());
+    data_path.push("tessdata");
+    return data_path;
+}
+
+async fn setup() {
     // Load .env
     dotenv().ok();
+    // Download tesseract model if it doesn't exist
+    let data_path = get_tessdata_path();
+    let data_file = data_path.join("eng.traineddata");
+    if !data_file.exists() {
+        println!("Could not find eng tesseract model at {}, downloading...", data_file.to_str().unwrap());
+        if !data_path.exists() {
+            std::fs::create_dir_all(&data_path).expect("Failed to create tessdata directory");
+        }
+        let url = Url::parse("https://github.com/tesseract-ocr/tessdata/raw/main/eng.traineddata").unwrap();
+        let response = reqwest::get(url).await;
+        let mut file = std::fs::File::create(&data_file).unwrap();
+        let mut content = std::io::Cursor::new(response.unwrap().bytes().await.unwrap()); 
+        std::io::copy(&mut content, &mut file).unwrap();
+        println!("Saved tesseract eng model to {}", data_file.to_str().unwrap());
+    } else {
+        println!("Found existing tesseract eng model at {}", data_file.to_str().unwrap());
+    }
+}
 
+#[tokio::main]
+async fn main() {
+    setup().await;
+    let ocr = Mutex::new(OCR::new(get_tessdata_path().to_str().unwrap(), &"eng"));
+    
     let mut hk = hotkey::Listener::new();
-    hk.register_hotkey(hotkey::modifiers::SHIFT |  hotkey::modifiers::SUPER, 'A' as u32, ||{
+    hk.register_hotkey(hotkey::modifiers::SHIFT |  hotkey::modifiers::SUPER, 'A' as u32, move || {
         let screenshot = capture_window_screenshot();
-        let ocr_result = perform_ocr(&screenshot);
-
-        // TODO Save clipboard contents and restore them after completion
+        let mut ocr = ocr.lock().unwrap();
+        let ocr_result= ocr.perform_ocr(&screenshot);
         execute_ctrl_a_c();
         match read_clipboard() {
             Some(s) => {
                 // Run complete_text async as sync
-                let rt = tokio::runtime::Runtime::new().unwrap();
-                let response = rt.block_on(complete_text(&s, &ocr_result));
-                println!("Response: {}", response);
-                update_clipboard(&response);
-                paste_clipboard();
-    
+                tokio::spawn(async move {
+                    let response = complete_text(&s, &ocr_result).await;
+                    println!("Response: {}", &response);
+                    update_clipboard(&response);
+                    paste_clipboard();
+                    // Restore clipboard contents
+                    update_clipboard(&s);
+                });
             },
             None => println!("Failed to read clipboard"),
         }
     }).unwrap();
-
+    println!("Ready!");
     hk.listen();
 }
